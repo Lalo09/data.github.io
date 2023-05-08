96a09c6201095ef629419f3971e6cf01961b75a6
lalo09 <jose960808@gmail.com>
2023-05-08 14:04:02 -0600

    Updates blog

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import openai
 from git import Repo 
 from pathlib import Path
+import shutil
+import os
+from bs4 import BeautifulSoup as Soup
 
 with open('../key') as f:
     key = f.readline()
@@ -26,12 +29,72 @@ def update_blog(commit_message='Updates blog'):
     origin = repo.remote(name='origin')
     origin.push()
 
-random_text_string = "New test!!!! alv"
 
-with open(PATH_TO_BLOG/"index.html",'w') as f:
-    f.write(random_text_string)
+def create_new_blog(title,content,cover_image):
+    cover_image = Path(cover_image)
 
-update_blog()
+    files = len(list(PATH_TO_CONTENT.glob("*.html")))
+    new_title = f"{files+1}.html"
+    path_to_new_content = PATH_TO_CONTENT/new_title
 
+    shutil.copy(cover_image,PATH_TO_CONTENT)
+
+    if not os.path.exists(path_to_new_content):
+        #Write a new HTML file
+        with open(path_to_new_content,'w') as f:
+            f.write("<!DOCTYPE html>\n")
+            f.write("<html>\n")
+            f.write("<head>\n")
+            f.write(f"<title>{title}</title>\n")
+            f.write("</head>\n")
+
+            f.write("<body>\n")
+            f.write(f"<img src='{cover_image.name}' alt='cover image'><br>\n")
+            f.write(f"<h1>{title}</h1>")
+            #OpenAI --> Completion GPT
+            f.write(content.replace('\n','<br>\n'))
+            f.write("<body>\n")
+            f.write("<html>\n")
+            print ("blog created")
+            return path_to_new_content
+    else:
+        raise FileExistsError("File already exists, please check again your name")
+
+def check_for_duplicate_links(path_to_new_conntent, links):
+    urls = [str(link.get('href')) for link in links]
+    content_path = str(Path(*path_to_new_content.parts[-2:]))
+
+    return content_path in urls
+
+def write_to_index(path_to_new_content):
+    with open(PATH_TO_BLOG/'index.html') as index:
+        soup = Soup(index.read())
+
+    links = soup.find_all()    
+    last_link = links[-1]
+
+    if check_for_duplicate_links(path_to_new_content,links):
+        raise ValueError("Links already exists!")
+    
+    link_to_new_blog = soup.new_tag("a",href=Path(*path_to_new_content.parts[-2:]))
+    link_to_new_blog.string = path_to_new_content.name.split('.')[0]
+    last_link.insert_after(link_to_new_blog)
+
+    with open(PATH_TO_BLOG/'index.html','w') as f:
+        f.write(str(soup.prettify(formatter='html')))
+
+#random_text_string = "New test!!!! alv"
+
+#with open(PATH_TO_BLOG/"index.html",'w') as f:
+ #   f.write(random_text_string)
+
+
+path_to_new_content = create_new_blog("Test title","wqdwefew ewfef","images.jpeg")
+print(path_to_new_content)
 #print(PATH_TO_CONTENT)
 
+#with open(PATH_TO_BLOG/"index.html") as index:
+ #   soup = Soup(index.read())
+
+write_to_index(path_to_new_content)
+update_blog()
